12b2159e1c3eafa9c352d7dc4d9354746cb741fe
bhavana <bhavana@netlify.com>
2022-10-03 12:54:01 -0400

    added a note for local development

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,7 @@
 describe('Demo site', ()=>{
     it('loads', ()=> {
         cy.visit('/')
+        // change this to cy.visit('http://localhost:3000) for running it locally 
         cy.contains('h1', 'This is a heading').should('be.visible')
     })
 })
