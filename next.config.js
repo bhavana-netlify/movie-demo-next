b9ceafa2430a4549d4433bf9e61d22daf4d6e9d1
Andrea N <andrea@narbot.com>
2022-10-12 09:00:19 -0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/contact-us',
+        destination: '/404',
+        permanent: false,
+      },
+    ]
+  },
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
