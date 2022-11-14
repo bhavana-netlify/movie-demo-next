64465739a09ea1e35d91db4c5f9de17bf7dd641f
bhavana-netlify <bhavana@netlify.com>
2022-11-14 15:58:18 -0500

    adding new ef

---
+++ netlify/edge-functions/geo.ts
@@ -0,0 +1,29 @@
+import { Context } from "https://edge.netlify.com";
+
+export default async (request: Request, context: Context) => {
+  // Here's what's available on context.geo
+
+  // context: {
+  //   geo: {
+  //     city?: string;
+  //     country?: {
+  //       code?: string;
+  //       name?: string;
+  //     },
+  //     subdivision?: {
+  //       code?: string;
+  //       name?: string;
+  //     },
+  //   }
+  // }
+
+  return context.json({
+    geo: context.geo,
+    header: request.headers.get("x-nf-geo"),
+  });
+};
+
+export const config: Config = () => ({
+	cache: "manual",
+	path: "/geo"
+})
